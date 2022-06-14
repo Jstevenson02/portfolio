d61b52c03822585226983f8ab8ec0f7d14bad6f8
Jacob <jacobryanstevenson@gmail.com>
2022-06-14 09:35:45 -0500

    fixin'

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -25,48 +25,51 @@ const About = () => {
     <>
       <div className="container about-page">
         <div className="text-zone">
-          <h1>
+          <h1 className="text-area2">
             <AnimatedLetters
               letterClass={letterClass}
               strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
               idx={15}
             />
           </h1>
-          <p>
-            I am a self-teaching fulltime student currently pursuing my
-            Associates of Applied Science. I run a side gig creating websites
-            for small businesses. I am expanding my toolkit and have gnact for
-            problem solving.
-          </p>
-          <p align="LEFT">
-            I'm self motivated, determined and eager to learn new technologies
-            one design problem at a time.
-          </p>
-          <p>
-            To describe myself in one sentence I would say I'm a gamer, a
-            programmer, learning and tech obsessed!
-          </p>
+          <div className="text-area">
+            <p>
+              I am a self-teaching fulltime student currently pursuing my
+              Associates of Applied Science. I run a side gig creating websites
+              for small businesses. I am expanding my toolkit and have gnact for
+              problem solving.
+            </p>
+            <p align="LEFT">
+              I'm self motivated, determined and eager to learn new technologies
+              one design problem at a time.
+            </p>
+            <p>
+              To describe myself in one sentence I would say I'm a gamer, a
+              programmer, learning and tech obsessed!
+            </p>
+          </div>
         </div>
-
-        <div className="stage-cube-cont">
-          <div className="cubespinner">
-            <div className="face1">
-              <FontAwesomeIcon icon={faSass} color="#DD0031" />
-            </div>
-            <div className="face2">
-              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-            </div>
-            <div className="face3">
-              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-            </div>
-            <div className="face4">
-              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-            </div>
-            <div className="face5">
-              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
-            </div>
-            <div className="face6">
-              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+        <div>
+          <div className="stage-cube-cont">
+            <div className="cubespinner">
+              <div className="face1">
+                <FontAwesomeIcon icon={faSass} color="#DD0031" />
+              </div>
+              <div className="face2">
+                <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+              </div>
+              <div className="face3">
+                <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+              </div>
+              <div className="face4">
+                <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+              </div>
+              <div className="face5">
+                <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+              </div>
+              <div className="face6">
+                <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+              </div>
             </div>
           </div>
         </div>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,54 +1,71 @@
-import { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import LogoTitle from '../../assets/images/logo-s.png'
-import AnimatedLetters from '../AnimatedLetters'
-import Logo from './Logo'
-import './index.scss'
-import Loader from 'react-loaders'
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import LogoTitle from '../../assets/images/logo-s.png';
+import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo';
+import './index.scss';
+import Loader from 'react-loaders';
 
 const Home = () => {
+  // Breaks the route for some reason
+  // useEffect(() => {
+  //     return setTimeout(() => {
+  //         setLetterClass('text-animate-hover')
+  //     }, 4000)
+  // }, [])
 
-    // Breaks the route for some reason
-    // useEffect(() => {
-    //     return setTimeout(() => {
-    //         setLetterClass('text-animate-hover')
-    //     }, 4000)
-    // }, [])
+  const [letterClass, setLetterClass] = useState('text-animate');
+  const nameArray = ['a', 'c', 'o', 'b'];
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ];
 
-    const [letterClass, setLetterClass] = useState('text-animate')
-    const nameArray = ['a', 'c', 'o', 'b']
-    const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _12`}>I'</span>
+            <span className={`${letterClass} _12`}>m</span>
+            <img src={LogoTitle} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>Frontend Developer / Javascript Novice / Student </h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
+      </div>
 
-    return (
-        <>
-            <div className="container home-page">
-                <div className="text-zone">
-                    <h1>
-                        <span className={letterClass}>H</span>
-                        <span className={`${letterClass} _12`}>i,</span>
-                        <br />
-                        <span className={`${letterClass} _12`}>I'</span>
-                        <span className={`${letterClass} _12`}>m</span>
-                        <img src={LogoTitle} alt="developer" />
-                        <AnimatedLetters letterClass={letterClass}
-                            strArray={nameArray}
-                            idx={15}
-                        />
-                        <br />
-                        <AnimatedLetters letterClass={letterClass}
-                            strArray={jobArray}
-                            idx={22}
-                        />
-                    </h1>
-                    <h2>Frontend Developer / Javascript Novice / Student </h2>
-                    <Link to="/contact" className='flat-button'>CONTACT ME</Link>
-                </div>
-                <Logo />
-
-            </div>
-
-            <Loader type='pacman' />
-        </>
-    )
-}
-export default Home
+      <Loader type="pacman" />
+    </>
+  );
+};
+export default Home;
